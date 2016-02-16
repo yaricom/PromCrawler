4ce3db88d26972226024dfbf08f28e23fa408e2f
Iaroslav Omelianenko <yaric.mail@gmail.com>
2016-02-17 00:37:37 +0200

    Implemeted full site crawling

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 	"net/http"
 	"os"
 	"strings"
 )
+
+// The structure to hold info about particular item
+type Item struct {
+	id      string
+	imgUrl  string
+	pageUrl string
+	title   string
+}
 
 // Helper function to pull the href attribute from a Token
 func getHref(t html.Token) (ok bool, href string) {
@@ -23,8 +32,41 @@ func getHref(t html.Token) (ok bool, href string) {
 	return
 }
 
+func getItemId(t html.Token) (ok bool, id string) {
+	for _, a := range t.Attr {
+		if a.Key == "title" {
+			id = a.Val
+			ok = true
+			break
+		}
+	}
+	return
+}
+
+func getImgSrc(t html.Token) (ok bool, src string) {
+	for _, a := range t.Attr {
+		if a.Key == "src" {
+			src = a.Val
+			ok = true
+			break
+		}
+	}
+	return
+}
+
+func getTitle(t html.Token) (ok bool, title string) {
+	for _, a := range t.Attr {
+		if a.Key == "title" {
+			title = a.Val
+			ok = true
+			break
+		}
+	}
+	return
+}
+
 // Extract all http** links from a given webpage
-func crawl(url string, ch chan string, chFinished chan bool) {
+func crawl(url string, ch chan Item, chFinished chan bool) {
 	resp, err := http.Get(url)
 
 	defer func() {
@@ -42,6 +84,10 @@ func crawl(url string, ch chan string, chFinished chan bool) {
 
 	z := html.NewTokenizer(b)
 
+	item := Item{}
+	idFound := false
+	imgFound := false
+
 	for {
 		tt := z.Next()
 
@@ -49,48 +95,78 @@ func crawl(url string, ch chan string, chFinished chan bool) {
 		case tt == html.ErrorToken:
 			// End of the document, we're done
 			return
+
+		case tt == html.SelfClosingTagToken:
+			t := z.Token()
+			// get next IMG
+			if idFound && t.DataAtom == atom.Img {
+				ok, src := getImgSrc(t)
+				if !ok {
+					continue
+				}
+				item.imgUrl = src
+				imgFound = true
+			}
+			break
+
 		case tt == html.StartTagToken:
 			t := z.Token()
 
-			// Check if the token is an <a> tag
-			isAnchor := t.Data == "a"
-			if !isAnchor {
-				continue
+			// check if at the beginning of interest's area
+			if t.DataAtom == atom.Span {
+				ok, id := getItemId(t)
+				if !ok {
+					continue
+				}
+				item.id = id
+				idFound = true
 			}
 
-			// Extract the href value, if there is one
-			ok, url := getHref(t)
-			if !ok {
-				continue
-			}
+			// get next <a>
+			if imgFound && t.DataAtom == atom.A {
+				// Extract the href value, if there is one
+				ok, url := getHref(t)
+				if !ok {
+					continue
+				}
+				item.pageUrl = url
 
-			// Make sure the url begines in http**
-			hasProto := strings.Index(url, "http") == 0
-			if hasProto {
-				ch <- url
+				ok, title := getTitle(t)
+				if ok {
+					item.title = title
+				}
+
+				// Make sure the url begines in http**
+				hasProto := strings.Index(url, "http") == 0
+				if hasProto {
+					ch <- item
+
+					idFound = false
+					imgFound = false
+				}
 			}
 		}
 	}
 }
 
 func main() {
-	foundUrls := make(map[string]bool)
+	var foundItems []Item
 	seedUrls := os.Args[1:]
 
 	// Channels
-	chUrls := make(chan string)
+	chItems := make(chan Item)
 	chFinished := make(chan bool)
 
 	// Kick off the crawl process (concurrently)
 	for _, url := range seedUrls {
-		go crawl(url, chUrls, chFinished)
+		go crawl(url, chItems, chFinished)
 	}
 
 	// Subscribe to both channels
 	for c := 0; c < len(seedUrls); {
 		select {
-		case url := <-chUrls:
-			foundUrls[url] = true
+		case item := <-chItems:
+			foundItems = append(foundItems, item)
 		case <-chFinished:
 			c++
 		}
@@ -98,11 +174,11 @@ func main() {
 
 	// We're done! Print the results...
 
-	fmt.Println("\nFound", len(foundUrls), "unique urls:\n")
+	fmt.Println("\nFound", len(foundItems), "unique urls:\n")
 
-	for url, _ := range foundUrls {
-		fmt.Println(" - " + url)
+	for _, item := range foundItems {
+		fmt.Printf("%s, %s, %s \n", item.id, item.pageUrl, item.title)
 	}
 
-	close(chUrls)
+	close(chItems)
 }
